59d4a6a521c5be5277c2b7ceb5816dee0c3f6280
alexlam184 <lamalexander1997@gmail.com>
2022-06-14 00:28:04 +0800

    feat : add redux storage

--- category-show/src/index.tsx
+++ category-show/src/index.tsx
@@ -4,13 +4,25 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk"
+import reducer from "./redux/reducer"
+import { createStore, applyMiddleware, Store } from "redux"
+
+// https://www.freecodecamp.org/news/how-to-use-redux-in-your-react-typescript-app/
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+      <Provider store={store}>
       <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
 );
