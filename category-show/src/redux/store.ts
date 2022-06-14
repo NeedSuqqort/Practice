8a98532c296b2a85f131409200027501dfb0a86d
alexlam184 <lamalexander1997@gmail.com>
2022-06-14 22:38:40 +0800

    chore : add react thunk

--- category-show/src/redux/store.ts
+++ category-show/src/redux/store.ts
@@ -3,14 +3,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from "redux-devtools-extension";
 import { ICart } from "./types";
 import userReducers from "./reducer";
+import reduxThunk from "redux-thunk"
+
+const middleware = [
+    reduxThunk,
+];
+
+// const reducers = combineReducers({ cart: userReducers });
+// export const store = createStore(reducers, composeWithDevTools(
+//   applyMiddleware(...middleware),
+//   // other store enhancers if any
+// ));
+
 const reducers = combineReducers({ cart: userReducers });
-export const store = createStore(reducers, composeWithDevTools());
-
-
-// export const store = configureStore({
-//   reducer: reducers,
-//   enhancers:[composeWithDevTools()]
-// });
+export const store = configureStore({
+  reducer: reducers,
+  middleware:middleware
+});
 
 
 
